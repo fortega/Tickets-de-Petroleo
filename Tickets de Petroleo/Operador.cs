b78909ffacd98920dac3ed6c016ed295c6577613
fortega <felipe@nosmintio.cl>
2010-09-23 09:55:28 -0400

    Operador.cs done

--- Tickets de Petroleo/Operador.cs
+++ Tickets de Petroleo/Operador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Tickets_de_Petroleo
 {
@@ -13,7 +14,16 @@ namespace Tickets_de_Petroleo
 
         public Operador(string nombre, string passwd)
         {
-            
+            Database db = new Database("SELECT * FROM operadores WHERE operador_nombre = @nombre AND operador_passwd = @passwd");
+            db.addParameter("nombre", System.Data.SqlDbType.VarChar, nombre);
+            db.addParameter("passwd", System.Data.SqlDbType.VarChar, passwd);
+
+            DataTable dt = db.getData();
+
+            if (dt.Rows.Count == 1)
+                setData(dt.Rows[0]);
+            else
+                throw new Exception("Nombre o Password incorrecto");
         }
 
         public Operador(string nombre, bool admin)
@@ -35,6 +45,12 @@ namespace Tickets_de_Petroleo
         public override string ToString()
         {
             return nombre;
+        }
+
+        private void setData(DataRow dr)
+        {
+            this.nombre = (string)dr["operador_nombre"];
+            this.admin = (bool)dr["operador_admin"];
         }
     }
 }
