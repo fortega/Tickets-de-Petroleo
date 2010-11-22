58db5e3bc483952e9199fec68a186e4cb3460c3b
Felipe M. Ortega Bustamante <felipe@nosmintio.cl>
2010-11-22 18:08:00 -0300

    Interfaces created

--- Tickets de Petroleo/ChoferesMain.Designer.cs
+++ Tickets de Petroleo/ChoferesMain.Designer.cs
@@ -28,11 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.layout = new System.Windows.Forms.TableLayoutPanel();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.pnlBotones = new System.Windows.Forms.Panel();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.layout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.pnlBotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.ColumnCount = 2;
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.layout.Controls.Add(this.gv, 0, 0);
+            this.layout.Controls.Add(this.pnlBotones, 1, 0);
+            this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layout.Location = new System.Drawing.Point(0, 0);
+            this.layout.Name = "layout";
+            this.layout.RowCount = 1;
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.layout.Size = new System.Drawing.Size(284, 262);
+            this.layout.TabIndex = 1;
+            // 
+            // gv
+            // 
+            this.gv.AllowUserToAddRows = false;
+            this.gv.AllowUserToDeleteRows = false;
+            this.gv.AllowUserToOrderColumns = true;
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gv.Location = new System.Drawing.Point(3, 3);
+            this.gv.MultiSelect = false;
+            this.gv.Name = "gv";
+            this.gv.ReadOnly = true;
+            this.gv.Size = new System.Drawing.Size(188, 452);
+            this.gv.TabIndex = 0;
+            // 
+            // pnlBotones
+            // 
+            this.pnlBotones.Controls.Add(this.btnBorrar);
+            this.pnlBotones.Controls.Add(this.btnEditar);
+            this.pnlBotones.Controls.Add(this.btnCrear);
+            this.pnlBotones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBotones.Location = new System.Drawing.Point(197, 3);
+            this.pnlBotones.Name = "pnlBotones";
+            this.pnlBotones.Size = new System.Drawing.Size(84, 452);
+            this.pnlBotones.TabIndex = 1;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(4, 70);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(4, 40);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(4, 10);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            // 
+            // ChoferesMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.layout);
+            this.Name = "ChoferesMain";
             this.Text = "ChoferesMain";
+            this.layout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.pnlBotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel layout;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.Panel pnlBotones;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnCrear;
     }
 }
