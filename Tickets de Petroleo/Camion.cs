22064ecc6729416ee437a3f5fd5de4fcc6004729
Felipe M. Ortega Bustamante <felipe@nosmintio.cl>
2010-11-23 17:25:45 -0300

    Working in Camion.cs

---
+++ Tickets de Petroleo/Camion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tickets_de_Petroleo
+{
+    class Camion
+    {
+        private Empresa empresa;
+        private string patente;
+
+        public Camion(Empresa empresa, string patente)
+        {
+            this.empresa = empresa;
+            this.patente = patente;
+        }
+    }
+}
